59527f868f18d49b9dd285a0a27579be0a9063e3
60gCreatine <cphah643@gmail.com>
2025-03-07 00:32:37 +0100

    function to see if opponent is in tri

--- Freezeframe.R
+++ Freezeframe.R
@@ -117,5 +117,57 @@ ggplot() +
 ##### if a0 > a then outside
 
 # PH og modstandere.
-is_opp
+
+
+# Funktion til at beregne trekantens areal
+calculate_triangle_area <- function(x1, y1, x2, y2, x3, y3) {
+  return(0.5 * abs(x1 * (y2 - y3) + x2 * (y3 - y1) + x3 * (y1 - y2)))
+}
+
+# Funktion til at teste, om en modstander er i sektoren
+is_opponent_inside_triangle <- function(o_x, o_y, shooter_x, shooter_y) {
+  # Målstolper (StatsBomb koordinater)
+  x1 <- 120  # Højre målstolpe
+  y1 <- 44
+  x2 <- 120  # Venstre målstolpe
+  y2 <- 36
+  x3 <- shooter_x
+  y3 <- shooter_y
   
+  # Beregn trekantens samlede areal
+  total_area <- calculate_triangle_area(x1, y1, x2, y2, x3, y3)
+  
+  # Beregn arealet af de tre deltrekanter
+  area1 <- calculate_triangle_area(o_x, o_y, x2, y2, x3, y3)
+  area2 <- calculate_triangle_area(x1, y1, o_x, o_y, x3, y3)
+  area3 <- calculate_triangle_area(x1, y1, x2, y2, o_x, o_y)
+  
+  # Sammenlign arealer (floating-point tolerance på 0.1)
+  return(abs((area1 + area2 + area3) - total_area) < 0.1)
+}
+
+# Brug df[2,] som skyder
+shooter_x <- df[2, "location.x"]
+shooter_y <- df[2, "location.y"]
+
+# Tjek modstandere i testff (hvor teammate == FALSE)
+opponents <- testff %>% filter(teammate == FALSE)
+
+# Tilføj en ny kolonne der viser, om modstanderen er i sektoren
+opponents <- opponents %>%
+  rowwise() %>%
+  mutate(in_triangle = is_opponent_inside_triangle(x, y, shooter_x, shooter_y))
+
+# Se antal modstandere i sektoren
+num_opponents_in_triangle <- sum(opponents$in_triangle)
+print(num_opponents_in_triangle)
+
+# plot til at se hvem der er foran den som skyder 
+ggplot() +
+  annotate_pitch(dimensions = pitch_statsbomb, colour = "white", fill = "#3ab54d") +
+  geom_polygon(data = tridf, aes(x = x, y = y), alpha = 0.4, fill = "blue") +
+  geom_point(data = opponents, aes(x = x, y = y, color = in_triangle), size = 2) +
+  scale_color_manual(values = c("TRUE" = "red", "FALSE" = "black")) +  # Røde modstandere i sektoren
+  theme_pitch() +
+  coord_flip(xlim = c(75, 121)) +
+  scale_y_reverse()
